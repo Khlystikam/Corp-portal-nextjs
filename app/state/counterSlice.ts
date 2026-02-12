66e23946be40ffba65134db6bf024348565b640c
Aleksandr Khlystik <aleksandr@dev-khlystikam.ru>
2026-02-13 01:26:00 +0300

    redux toolkit p5%

---
+++ app/state/counterSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+const counterReducer = createSlice({
+	name: "counter",
+	initialState: { value: 0 },
+	reducers: {
+		increment: (state) => {
+			state.value += 1;
+		},
+	},
+});
+export const { increment } = counterReducer.actions;
+export default counterReducer.reducer;
